57ab9adc44257f5c0ee759fa874c125fba09e962
vzhV <volodymyr.zherdetskyi.knm.2020@edu.lpnu.ua>
2022-08-10 19:37:27 +0300

    First commit

---
+++ src/main/java/com/vzh/iherych/IherychApplication.java
@@ -0,0 +1,13 @@
+package com.vzh.iherych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IherychApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IherychApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vzh/iherych/IherychApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vzh.iherych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IherychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
