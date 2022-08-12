582fafd86f3ed180c9c70cc4d315171eb377287f
vzhV <91687684+vzhV@users.noreply.github.com>
2022-08-12 14:18:26 +0300

    Change password (#2)
    * Access and refresh tokens
    
    * Change password

--- src/main/java/com/vzh/iherych/Filter/CustomAuthorizationFilter.java
+++ src/main/java/com/vzh/iherych/Filter/CustomAuthorizationFilter.java
@@ -26,7 +26,8 @@ import static org.springframework.http.HttpStatus.FORBIDDEN;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if(request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/token/refresh")) {
+
+        if(request.getServletPath().equals("/api/user/login")) {
             filterChain.doFilter(request, response);
         }
         else{
@@ -57,7 +58,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
             }
             else{
                 log.error("Access token is null");
-                response.sendRedirect("/api/login");
+                response.sendRedirect("/login");
             }
         }
     }
--- src/main/java/com/vzh/iherych/Security/SecurityConfig.java
+++ src/main/java/com/vzh/iherych/Security/SecurityConfig.java
@@ -34,12 +34,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter authenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-        authenticationFilter.setFilterProcessesUrl("/api/login");
+        authenticationFilter.setFilterProcessesUrl("/api/user/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers(GET, "/api/user/email/**").hasAuthority("ADMIN");
-        http.authorizeRequests().antMatchers(GET, "/api/user/username/**").hasAnyAuthority("ADMIN","SUPER_ADMIN");
+        http.authorizeRequests().antMatchers(GET, "/api/user/username/**").hasAnyAuthority("ADMIN", "SUPER_ADMIN");
+
+        http.formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .permitAll();
+
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .deleteCookies("access_token")
+                .deleteCookies("refresh_token");
+
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(authenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
